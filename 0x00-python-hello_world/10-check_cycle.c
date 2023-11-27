e2f8463e10e0a633f2604a4ef10a24d3dc356136
Maggie-Njeri <mkiri00@gmail.com>
2023-11-27 10:06:37 -0800

    python rocks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,22 +1,25 @@
 #include "lists.h"
 /**
- * check_cycle - to check if there is a cycle
- * @list: is the linked list to be checked
- * Return: success (1), failure (0)
+ *
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *fast;
+	listint_t  *slow;
 
-	if (!list)
-		return(0);
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->fast->next;
-		if (slow == fast)
-			return(1);
-	}
-	return (0);
+	 if (!list || !list->next)
+		 return (0);
+	 fast = list->next->next;
+	 slow = list;
+
+	 while (slow != NULL && fast !=NULL && fast->next !' NULL)
+	 {
+		 slow = slow->next;
+		 fast - fast->next->next;
+		 if (slow == fast)
+		 {
+			 return (1);
+		 }
+	 }
+		 return (0);
 }
