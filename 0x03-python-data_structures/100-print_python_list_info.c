235d9fff5b30079d4907da412f64a981cce2a772
Maggie-Njeri <mkiri00@gmail.com>
2023-12-03 11:19:39 -0800

    lists, tuples are data structures

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <python.h>
+#include </usr/include/python3.9/pyconfig-64.h>
+
+
+/**
+ * print_python_list_info -  function that prints some basic
+ *		info about Python lists
+ * @p: python list
+ */
+
+
+void print_python_list_info(PyObject *p)
+{
+	int x;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (x = 0; x < Py_SIZE(p); x++)
+		printf("Element %d: %s\n", x, Py_TYPE(PyList_GetItem(p, x))->tp_name);
+}
