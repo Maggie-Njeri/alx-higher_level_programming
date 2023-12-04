9bf141b0db0a63901cbc84254c5079155497b999
Maggie-Njeri <mkiri00@gmail.com>
2023-12-04 13:31:56 -0800

    lists, tuples and dictionaries

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,8 +2,6 @@
 #define LISTS_H
 
 #include <stddef.h>
-#include <Python.h>
-#include </usr/include/python3.9/pyconfig-64.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,7 +20,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-void print_python_list_info(PyObject *p);
+
 int is_palindrome(listint_t **head);
 
 #endif
