bb0a0d285c4a4c16cee611d8ae49e92d53968fa7
Maggie-Njeri <mkiri00@gmail.com>
2023-12-04 12:45:43 -0800

    lists, tuples and dictionaries

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,24 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <python.h>
-#include </usr/include/python3.4/pyconfig-64.h>
-
 
 /**
+ *
  * print_python_list_info -  function that prints some basic
- *		info about Python lists
+ *			info about Python lists
  * @p: python list
  */
 
-
 void print_python_list_info(PyObject *p)
 {
-	int x;
+	long int size = PyList_Size(p);
+	int elem;
+	PyListObject *obj = (PyListObject)p;
 
 	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (x = 0; x < Py_SIZE(p); x++)
-		printf("Element %d: %s\n", x, Py_TYPE(PyList_GetItem(p, x))->tp_name);
+	for (elem = 0; elem < Py_SIZE(p); elem++)
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
 }
--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,20 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-x = ['hello', 'World']
-lib.print_python_list_info(x)
-del x[1]
-lib.print_python_list_info(x)
-x = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
-lib.print_python_list_info(x)
-x = []
-lib.print_python_list_info(x)
-x.append(0)
-lib.print_python_list_info(x)
-x.append(1)
-x.append(2)
-x.append(3)
-x.append(4)
-lib.print_python_list_info(x)
-x.pop()
-lib.print_python_list_info(x)
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
