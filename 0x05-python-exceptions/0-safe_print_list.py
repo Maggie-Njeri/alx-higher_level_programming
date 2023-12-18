7e51bbea47f384edfae64fc6a301a6d2f97aed36
Maggie-Njeri <mkiri00@gmail.com>
2023-12-18 13:15:02 -0800

    python is cool??

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    numero = 0
-    for a in range(x):
+    bet = 0
+    for i in range(x):
         try:
-            print(my_list[a], end="")
-            numero += 1
+            print("{}".format(my_list[i]), end="")
+            bet += 1
         except IndexError:
             break
     print("")
-    return
+    return (bet)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    list_m = 0
-    for a in range(x):
+    bet = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[a]), end="")
-            list_m += 1
+            print("{:d}".format(my_list[i]), end="")
+            bet += 1
         except (ValueError, TypeError):
-            pass
-    print()
-    return
+            continue
+    print("")
+    return (bet)
