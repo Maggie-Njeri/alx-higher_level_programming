bf1ce6aad2ca32bed20356653c25ffeb3200a395
Maggie-Njeri <mkiri00@gmail.com>
2023-12-18 12:34:05 -0800

    python is cool??

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    numero = 0
+    for a in range(x):
+        try:
+            print(my_list[i], end="")
+            numero += 1
+        except IndexError:
+            break
+    print("")
+    Return
