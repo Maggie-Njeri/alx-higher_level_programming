99d9e58254f2710e35746e84d507d1e9e42e1eb5
Maggie-Njeri <mkiri00@gmail.com>
2023-12-18 12:40:32 -0800

    python is cool??

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     numero = 0
     for a in range(x):
         try:
-            print(my_list[i], end="")
+            print(my_list[a], end="")
             numero += 1
         except IndexError:
             break
